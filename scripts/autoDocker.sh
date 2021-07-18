b9ce4ea1301f9110f88bf6ca937fb42d338d4a5e
PavelShyshkavets <Pavel_Shyshkavets@epam.com>
2021-07-18 15:57:59 +0300

    Delete scripts
    Delete scripts.

--- scripts/autoDocker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker stop $(docker ps -a -q)
-docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) https://028922724832.dkr.ecr.us-east-1.amazonaws.com
-docker pull 028922724832.dkr.ecr.us-east-1.amazonaws.com/book-api-repo
-docker run -it -p 80:80 028922724832.dkr.ecr.us-east-1.amazonaws.com/book-api-repo
